d145d0b526dd58f10b719e3fe962fe8de80d94ab
yugal47 <yugaltiwari47@gmail.com>
2026-05-26 23:39:15 +0530

    added backend logic inside backend dir

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Backend is running"}
+    return {"message": "Backend is running "}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
