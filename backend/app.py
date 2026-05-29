039dbe83c2ccd06eaf42d9ef9f4b5fe738c4c3fb
yugal47 <yugaltiwari47@gmail.com>
2026-05-29 13:21:09 +0530

    Small changes

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return {"message": "Backend is running"}
 
+@app.route("/api")
+def api():
+    return {"status": "backend working"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
